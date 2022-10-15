e212719ed0b06279dfe914cb920ab0e527f248aa
EmreAkyoll <78498301+EmreAkyoll@users.noreply.github.com>
2022-10-15 20:47:45 +0300

    Update siteVeri.js

--- siteVeri.js
+++ siteVeri.js
@@ -17,36 +17,36 @@ async function scrapeProduct(url) {
   console.log("emre")
   
 
-  switch (new Date().getDay()) {
-    case 0:
-      day = "Sunday";
-     break;
-    case 1:
-      day = "Monday";
-       [el] = await page.$x('//h3[contains(.,"Pazartesi")]/ancestor::tbody[1]'); //PAZARTESİ YEMEKLERİ
-      break;
-    case 2:
-       day = "Tuesday";
-       [el] = await page.$x('//h3[contains(.,"Salı")]/ancestor::tbody[1]'); //SALI YEMEKLERİ 
-      break;
-    case 3:
-      day = "Wednesday";
-       [el] = await page.$x('//h3[contains(.,"Çarşama")]/ancestor::tbody[1]'); //ÇARŞAMBA YEMEKLERİ
-      break;
-    case 4:
-      day = "Thursday";
-       [el] = await page.$x('//h3[contains(.,"Perşembe")]/ancestor::tbody[1]'); //PERŞEMBE YEMEKLERİ
-      break;
-    case 5:
-      day = "Friday";
-       [el] = await page.$x('//h3[contains(.,"Cuma")]/ancestor::tbody[1]'); //CUMA YEMEKLERİ
-      break;
-    case 6:
-      day = "Saturday";
-  }
+ // switch (new Date().getDay()) {
+ //   case 0:
+ //     day = "Sunday";
+ //    break;
+ //   case 1:
+ //     day = "Monday";
+ //      [el] = await page.$x('//h3[contains(.,"Pazartesi")]/ancestor::tbody[1]'); //PAZARTESİ YEMEKLERİ
+ //     break;
+ //   case 2:
+ //      day = "Tuesday";
+//       [el] = await page.$x('//h3[contains(.,"Salı")]/ancestor::tbody[1]'); //SALI YEMEKLERİ 
+ //     break;
+ //   case 3:
+ //     day = "Wednesday";
+ //      [el] = await page.$x('//h3[contains(.,"Çarşama")]/ancestor::tbody[1]'); //ÇARŞAMBA YEMEKLERİ
+ //     break;
+ //   case 4:
+ //     day = "Thursday";
+ //      [el] = await page.$x('//h3[contains(.,"Perşembe")]/ancestor::tbody[1]'); //PERŞEMBE YEMEKLERİ
+ //     break;
+ //   case 5:
+ //     day = "Friday";
+ //      [el] = await page.$x('//h3[contains(.,"Cuma")]/ancestor::tbody[1]'); //CUMA YEMEKLERİ
+ //     break;
+ //  case 6:
+ //     day = "Saturday";
+ // }
   // //*[@id="icerik"]/div/table/tbody/tr[4]/td/table/tbody
   
-  //[el] = await page.$x('//*[@id="productTitle"]'); //PAZARTESİ
+  [el] = await page.$x('//*[@id="productTitle"]'); //amazon
   
   
   const src = await el.getProperty("textContent");
