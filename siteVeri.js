d0d88dc84d84f5c4abea3414cbe6aea0fa74121d
EmreAkyoll <78498301+EmreAkyoll@users.noreply.github.com>
2022-10-15 22:51:24 +0300

    Update siteVeri.js

--- siteVeri.js
+++ siteVeri.js
@@ -20,48 +20,48 @@ async function scrapeProduct(url) {
   console.log("emre")
   
 
- // switch (new Date().getDay()) {
- //   case 0:
- //     day = "Sunday";
- //    break;
- //   case 1:
- //     day = "Monday";
- //      [el] = await page.$x('//h3[contains(.,"Pazartesi")]/ancestor::tbody[1]'); //PAZARTESİ YEMEKLERİ
- //     break;
- //   case 2:
- //      day = "Tuesday";
-//       [el] = await page.$x('//h3[contains(.,"Salı")]/ancestor::tbody[1]'); //SALI YEMEKLERİ 
- //     break;
- //   case 3:
- //     day = "Wednesday";
- //      [el] = await page.$x('//h3[contains(.,"Çarşama")]/ancestor::tbody[1]'); //ÇARŞAMBA YEMEKLERİ
- //     break;
- //   case 4:
- //     day = "Thursday";
- //      [el] = await page.$x('//h3[contains(.,"Perşembe")]/ancestor::tbody[1]'); //PERŞEMBE YEMEKLERİ
- //     break;
- //   case 5:
- //     day = "Friday";
- //      [el] = await page.$x('//h3[contains(.,"Cuma")]/ancestor::tbody[1]'); //CUMA YEMEKLERİ
- //     break;
- //  case 6:
- //     day = "Saturday";
- // }
+  switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+     break;
+    case 1:
+      day = "Monday";
+       [el] = await page.$x('//h3[contains(.,"Pazartesi")]/ancestor::tbody[1]'); //PAZARTESİ YEMEKLERİ
+      break;
+    case 2:
+       day = "Tuesday";
+       [el] = await page.$x('//h3[contains(.,"Salı")]/ancestor::tbody[1]'); //SALI YEMEKLERİ 
+      break;
+    case 3:
+      day = "Wednesday";
+       [el] = await page.$x('//h3[contains(.,"Çarşama")]/ancestor::tbody[1]'); //ÇARŞAMBA YEMEKLERİ
+      break;
+    case 4:
+      day = "Thursday";
+       [el] = await page.$x('//h3[contains(.,"Perşembe")]/ancestor::tbody[1]'); //PERŞEMBE YEMEKLERİ
+      break;
+    case 5:
+      day = "Friday";
+       [el] = await page.$x('//h3[contains(.,"Cuma")]/ancestor::tbody[1]'); //CUMA YEMEKLERİ
+      break;
+   case 6:
+      day = "Saturday";
+  }
   // //*[@id="icerik"]/div/table/tbody/tr[4]/td/table/tbody
   
- const [el] = await page.$x('//*[@id="productTitle"]'); //amazon
+ //const [el] = await page.$x('//*[@id="productTitle"]'); //amazon deneme
   
   
   const src = await el.getProperty("textContent");
   var srcTxt = await src.jsonValue();
 
- // var kcalValue = /\d+ Kcal/.exec(srcTxt)
- // srcTxt ="\n"+ srcTxt.replace(kcalValue,"")
+  var kcalValue = /\d+ Kcal/.exec(srcTxt)
+  srcTxt ="\n"+ srcTxt.replace(kcalValue,"")
 
- // srcTxt = srcTxt.split("\n").map(x=>"  "+x.trim()).join("\n");
+  srcTxt = srcTxt.split("\n").map(x=>"  "+x.trim()).join("\n");
 
 
- // srcTxt += '\n'+"     "+kcalValue
+  srcTxt += '\n'+"     "+kcalValue
 
   return srcTxt;
 }
